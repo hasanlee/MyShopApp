41838d0fea1efcaf1b6453f96f7273329216b425
Hasanli Hasan <hesenli.hesen@gmail.com>
2017-05-22 13:11:16 +0400

    Initial commit

---
+++ app/src/main/java/com/hasanli/myshopapp/Config.java
@@ -0,0 +1,19 @@
+package com.hasanli.myshopapp;
+
+/**
+ * Created by hasan on 22.05.2017.
+ */
+
+public class Config {
+    //JSON URL
+    public static final String DATA_URL = "http://192.168.1.109/www/json.php";
+
+    //Tags used in the JSON String
+    public static final String TAG_USERNAME = "username";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_COURSE = "course";
+    public static final String TAG_SESSION = "session";
+
+    //JSON array name
+    public static final String JSON_ARRAY = "result";
+}
